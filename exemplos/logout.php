89f82f9160eb4e5eb6ce81a2daf9c12c09ffa64e
Paulo Sérgio Duff <paulosergioduff@gmail.com>
2017-05-23 14:47:10 -0300

    Mudança na sessão de login para status também
    ...nesse arquivo também

--- exemplos/logout.php
+++ exemplos/logout.php
@@ -5,7 +5,7 @@ Pequena demonstração de logoff usando sesssões.
 
 */
 session_start();
-unset($_SESSION['login']);
+unset($_SESSION['status']);
 echo "você foi deslogado!<br>";
 echo "<br><a href='logar.php'>Logar</a><a href='logout.php'>  deslogar</a><a href='restrito.php'>  Restrito</a>";
 ?>
